4c48f9c6bf6f30186c184e62f75366d5859d79ba
jeremy da silva <jeremydasilva1@icloud.com>
2024-01-11 19:30:42 +0100

    exercice2

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,11 @@
+class QuestionsController < ApplicationController
+
+  def ask
+    # Action logic (no instance variable needed for now)
+  end
+
+  def answer
+    # Action logic to calculate @answer based on params[:question]
+  end
+
+end
